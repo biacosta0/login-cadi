1c06cdfb6298b5a470d78affcfa10a81d2cac402
BiaCosta0 <farias.costa@alunoescolassociais.org.br>
2024-07-04 12:39:08 -0300

    inserir nome dentro do array

---
+++ login.html/controller.js
@@ -0,0 +1,32 @@
+
+//VALIDAÇÃO DE PREENCHIMENTOet
+function acessar(){
+    let loginEmail = document.getElementById('loginEmail').value;
+    let loginSenha = document.getElementById('loginSenha').value;
+ 
+    if(!loginEmail || !loginSenha){
+        alert('Favor preencher todos os campos');
+    } else{
+       // alert("Campos preenchidos com sucesso");
+       window.location.href = 'cadastro.html';
+    }
+}
+ 
+//FUNÇÃO DE CRIAÇÃO DE ARRAY PARA ARMAZENAMENTO DE NOMES
+ 
+var dadosLista = [];
+ 
+function salvarUser(){
+    let nomeUser = document.getElementById('nomeUser').value;
+ 
+    if(nomeUser){
+      dadosLista.push(nomeUser);
+      console.log(dadosLista);
+      document.getElementById('nomeUser').value = "";
+
+    } else{
+        alert("Favor, informar um nome para cadastro");
+    }
+}
+
+ 
