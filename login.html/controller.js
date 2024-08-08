a5c09668bc21713dc7b960ae418fc221300d8fd4
BiaCosta0 <farias.costa@alunoescolassociais.org.br>
2024-08-08 08:45:07 -0300

    coleta dos valores no campo

--- login.html/controller.js
+++ login.html/controller.js
@@ -35,7 +35,7 @@ function salvarUser(){
    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th>";
   //LAÇO DE REPETIÇÃO
    for(let i = 0; i <=(dadosLista.length - 1); i ++){
-     tabela += "<tr><td>" + dadosLista[i] + "</td><td> <button onclick=''</button></td></tr>";
+     tabela += "<tr><td>" + dadosLista[i] + "</td><td> <button onclick='editar(this.parenTnode.rowIndex)'> Editar </button> <button onclick='excluir(this.parenTnode.rowIndex)'> Excluir </button></td></tr>";
      document.getElementById('tabela').innerHTML = tabela;
    }
   }
@@ -46,4 +46,11 @@ function salvarUser(){
     dadosLista.splice((i - 1), 1);
     document.getElementById('tabela').deleteRow(i);
 
+  }
+
+  //FUNÇAÕ EDITAR NOME DA LISTA
+
+  function editar(i){
+    document.getElementById("nomeUser").value = dadosLista[(i - 1)];
+    dadosLista.splice(dadosLista[(i - 1)], 1);
   }
